695b2964310fb6f49f9c5859495afd084eafe212
David Odoh <david.odoh1@gmail.com>
2018-10-06 10:12:48 +0100

    Added Nav

--- src/containers/Nav/nav.js
+++ src/containers/Nav/nav.js
@@ -4,8 +4,19 @@ export default class nav extends Component {
     render() {
         return (
             <div>
-                Nav works
-      </div>
+                <div className="alt">
+                    <div className="tabs home_tabs">
+                        <ul className="row">
+                            <li>Background Info</li>
+                            <li>News</li>
+                            <li>Videos</li>
+                            <li>Xperience Seedoo.tv</li>
+                            <li>Contact Us</li>
+                            <li>Mobile App Download</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
