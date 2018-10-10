ec9bd2ff4d260a174dbf91004d74a0d1cf819502
david-odoh <david.odoh1@gmail.com>
2018-10-10 17:28:04 +0100

    Added a tile header

--- src/UI-Helpers/tile/tile.js
+++ src/UI-Helpers/tile/tile.js
@@ -6,7 +6,13 @@ export default class tile extends Component {
     render() {
         return (
             <div>
-                Tile1
+                <div className="tile">
+                    <div className="title-head">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" alt="Card image cap" />
+                    </div>
+                    <div className="tile body">
+                    </div>
+                </div>
             </div>
         )
     }
